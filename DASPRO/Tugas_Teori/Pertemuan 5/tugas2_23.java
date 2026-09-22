02ee59abbdcf61f13bac451bbde78433229837f2
muhammadpanjitaqiyuddin-wq <muhammadpanjitaqiyuddin@gmail.com>
2026-09-22 11:26:02 +0700

    Add files via upload

---
+++ DASPRO/Tugas_Teori/Pertemuan 5/tugas2_23.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class tugas2_23 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Masukkan kode layanan : ");
+    int layanan = input.nextInt();
+
+    switch (layanan) {
+      case 1:
+        System.out.println("Layanan : Legalisir Ijazah");
+        System.out.println("Loket : A");
+        break;
+      case 2:
+        System.out.println("Layanan : Surat Keterangan Aktif Kuliah");
+        System.out.println("Loket : B");
+        break;
+      case 3:
+        System.out.println("Layanan : Pembayaran UKT");
+        System.out.println("Loket : C");
+        break;
+      case 4:
+        System.out.println("Layanan : Pengajuan Cuti Akademik");
+        System.out.println("Loket : D");
+        break;
+      default:
+        System.out.println("Kode layanan tidak valid");
+    }
+  }
+}
